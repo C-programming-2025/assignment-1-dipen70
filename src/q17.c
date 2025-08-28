9e951f3724463e5386fa0388330db552adc75978
dipen70 <dipenbhattarai07@gmail.com>
2025-08-28 22:24:14 +0545

    push

--- src/q17.c
+++ src/q17.c
@@ -1,12 +1,11 @@
+
 #include<stdio.h>
 int main(){ 
     char c;
+    int result;
     printf("Enter a character: ");
     scanf("%c", &c);
-    if(c >= 'A' && c <= 'Z'){
-        printf("%c is an uppercase letter.\n", c);
-    } else {
-        printf("%c is not an uppercase letter.\n", c);
-    }
+    result=(c >= 'A' && c <= 'Z');
+    printf("%d" , result);
     return 0;
 }
