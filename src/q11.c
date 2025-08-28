971f45e29024b97bd5118a9d35f9f38607c65251
dipen70 <dipenbhattarai07@gmail.com>
2025-08-28 21:45:28 +0545

    submission_01

--- src/q11.c
+++ src/q11.c
@@ -3,7 +3,7 @@ int main(){
     int a, results;
     printf("enter an integer: ");
     scanf("%d", &a);
-    results= (a++) *3 -10;
+    results= ((a++) *3 )-10;
     printf("%d", results);
    
 }
